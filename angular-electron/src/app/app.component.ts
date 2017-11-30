7f4f08650120fdbb987dcc70f2c6de2b1ba5481a
Dan Wolf <dcwolf@gmail.com>
2017-11-30 16:51:51 -0500

    add remote call from render process to electron

--- angular-electron/src/app/app.component.ts
+++ angular-electron/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ElectronService } from 'ngx-electron';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  appData = '';
+
+  constructor(private _electronService: ElectronService) { }
+electro
+  ngOnInit () {
+    this.appData = this._electronService.remote.app.getPath('userData');
+  }
 }
