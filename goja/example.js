fffc354539c25c5802a78b508c1483bde13006ca
Dan Wolf <dcwolf@gmail.com>
2018-06-18 18:25:53 -0400

    Goja example

---
+++ goja/example.js
@@ -0,0 +1,4 @@
+//console.log('where am I?!?')
+//console.log(new Date().toGMTString())
+//console.log(new Date().toUTCString())
+console.log(new Date())
---
+++ goja/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dop251/goja"
+)
+
+func main() {
+	vm := goja.New()
+	v, err := vm.RunString("2 + 2")
+	if err != nil {
+		panic(err)
+	}
+	if num := v.Export().(int64); num != 4 {
+		panic(num)
+	} else {
+		fmt.Println(num)
+	}
+}
